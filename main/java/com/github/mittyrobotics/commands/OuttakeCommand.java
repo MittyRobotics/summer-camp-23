bfc1e66fcab8fa2480506ed60a15994f38cde31d
Naomi Carvalho <58865941+sherlocknomes@users.noreply.github.com>
2023-11-21 17:00:26 -0800

    final summer camp
    Completed summer camp code. Maintain for 2024 & 2025 until Romi Robot Systems are updated.

---
+++ main/java/com/github/mittyrobotics/commands/OuttakeCommand.java
@@ -0,0 +1,41 @@
+package com.github.mittyrobotics.commands;
+
+import com.github.mittyrobotics.OI;
+import com.github.mittyrobotics.subsystems.DriveTrainSubsystem;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class OuttakeCommand extends CommandBase {
+    /********************************************
+     *** HIT BOTH TRIGGERS TO EXIT TANK DRIVE ***
+     ********************************************/
+
+    private double leftPower, rightPower;
+    private boolean isRunning;
+
+    public OuttakeCommand() {
+        addRequirements(DriveTrainSubsystem.getInstance());
+    }
+
+    @Override
+    public void initialize() {
+        DriveTrainSubsystem.getInstance().setRoller(0);
+    }
+
+    @Override
+    public void execute() {
+
+        DriveTrainSubsystem.getInstance().setRoller(-0.5);
+
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        DriveTrainSubsystem.getInstance().setRoller(0);
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
---
+++ main/java/com/github/mittyrobotics/commands/TankDriveCommand.java
@@ -0,0 +1,56 @@
+package com.github.mittyrobotics.commands;
+
+import com.github.mittyrobotics.OI;
+import com.github.mittyrobotics.subsystems.DriveTrainSubsystem;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class TankDriveCommand extends CommandBase {
+    /********************************************
+     *** HIT BOTH TRIGGERS TO EXIT TANK DRIVE ***
+     ********************************************/
+
+    private double leftPower, rightPower;
+    private boolean isRunning;
+
+    public TankDriveCommand() {
+        addRequirements(DriveTrainSubsystem.getInstance());
+    }
+
+    @Override
+    public void initialize() {
+        //   System.out.println("tank drive command has been initialized");
+
+        DriveTrainSubsystem.getInstance().setMotors(0, 0);
+    }
+
+    @Override
+    public void execute() {
+        //  System.out.println("tank drive command is executing");
+
+        double percent = 0.5;
+        double deadzone = 0.1;
+        double left = OI.getInstance().getXboxController().getLeftY()* percent;
+        double right = OI.getInstance().getXboxController().getRightY() * percent;
+        if(Math.abs(left) < deadzone) {
+            left = 0;
+        }
+        if(Math.abs(right) < deadzone) {
+            right = 0;
+        }
+        DriveTrainSubsystem.getInstance().setMotors(left, right);
+
+
+
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        DriveTrainSubsystem.getInstance().setMotors(0, 0);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+}
